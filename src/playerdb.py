65a58ffde41efb8268834bec30407d47efe91fc2
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-23 15:52:47 +0300

    Update playerdb.py

--- src/playerdb.py
+++ src/playerdb.py
@@ -1,23 +1,21 @@
-from requests import get
+from requests import Session
 
-class PlayerDB:
-	def __init__(self) -> None:
-		self.api = "https://playerdb.co/api"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
-		}
-	
-	def minecraft_account_lookup(self, username: str) -> dict:
-		return get(
-			f"{self.api}/player/minecraft/{username}",
-			headers=self.headers).json()
-		
-	def steam_account_lookup(self, steam_id: str) -> dict:
-		return get(
-			f"{self.api}/player/steam/{steam_id}",
-			headers=self.headers).json()
-	
-	def xbox_account_lookup(self, username: str) -> dict:
-		return get(
-			f"{self.api}/player/xbox/{username}",
-			headers=self.headers).json()
+class PlayerDb:
+    def __init__(self) -> None:
+        self.api = "https://playerdb.co/api"
+        self.session = Session()
+        self.session.headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
+        }
+    
+    def minecraft_account_lookup(self, username: str) -> dict:
+        return self.session.get(
+            f"{self.api}/player/minecraft/{username}").json()
+        
+    def steam_account_lookup(self, steam_id: str) -> dict:
+        return self.session.get(
+            f"{self.api}/player/steam/{steam_id}").json()
+    
+    def xbox_account_lookup(self, username: str) -> dict:
+        return self.session.get(
+            f"{self.api}/player/xbox/{username}").json()
