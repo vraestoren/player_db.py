28294269868d7d5fc738e0f18499d02817ab2bc8
huok <77536370+vraestoren@users.noreply.github.com>
2026-03-25 11:47:07 +0300

    Update playerdb.py

--- src/playerdb.py
+++ src/playerdb.py
@@ -7,15 +7,15 @@ class PlayerDb:
         self.session.headers = {
             "User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
         }
-    
+
+    def _get(self, endpoint: str) -> dict:
+        return self.session.get(f"{self.api}{endpoint}").json()
+
     def minecraft_account_lookup(self, username: str) -> dict:
-        return self.session.get(
-            f"{self.api}/player/minecraft/{username}").json()
-        
+        return self._get(f"/player/minecraft/{username}")
+
     def steam_account_lookup(self, steam_id: str) -> dict:
-        return self.session.get(
-            f"{self.api}/player/steam/{steam_id}").json()
-    
+        return self._get(f"/player/steam/{steam_id}")
+
     def xbox_account_lookup(self, username: str) -> dict:
-        return self.session.get(
-            f"{self.api}/player/xbox/{username}").json()
+        return self._get(f"/player/xbox/{username}")
